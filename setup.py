70b3bd1102985963ee8769ec274b7eb255cccf9d
eberjoe <eberjoe@gmail.com>
2020-05-22 11:28:16 -0300

    Correct OS distribution

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ REQUIREMENTS = [
 CLASSIFIERS = [
     'Programming Language :: Python :: 3',
     'License :: OSI Approved :: MIT License',
-    'Operating System :: Windows',
+    'Operating System :: OS Independent',
 ]
 
 setup(
